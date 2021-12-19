8ae8970a09af57b82ddefb8c160b885ca6815608
Tahsin Masruf <tahsinpavel@gmail.com>
2021-12-20 01:42:50 +0600

    updating

--- multiple_Linear_Regression.py
+++ multiple_Linear_Regression.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 
+#Libraries 
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#data preprocessing
+
+data = pd.read_csv('50_Startups.csv')
+x = data.iloc[:, :-1].values
+y = data.iloc[:, 4].values
+
+#Encoding Catagorical Data
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
--- simple_Linear_Regression.py
+++ simple_Linear_Regression.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+#Libraries 
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
