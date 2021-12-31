f01c217b3c77578d14a65f4749d00d98e22264ea
Tahsin Masruf <tahsinpavel@gmail.com>
2021-12-31 12:38:02 +0600

    updating

--- multiple_Linear_Regression.py
+++ multiple_Linear_Regression.py
@@ -28,3 +28,10 @@ x = x[:,1:]
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x, y, test_size= 1/3, random_state= 0)
 
+#Fitting Multiple linear regression to training set
+from sklearn.linear_model import LinearRegression
+regressior = LinearRegression()
+regressior.fit(x_train,y_train)
+
+#Predicting
+y_pred = regressior.predict(x_test)
